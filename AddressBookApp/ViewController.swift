affbfb54fed88d431b885db9ac4308dab0730ae9
zehragizem <zgduman@gmail.com>
2025-04-13 02:28:41 +0300

    Search Bar added.

--- AddressBookApp/ViewController.swift
+++ AddressBookApp/ViewController.swift
@@ -8,33 +8,66 @@
 import UIKit
 import EFQRCode
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, PersonCellDelegate {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, PersonCellDelegate, UISearchBarDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
 
-    var people: [Person] = []
+    var people: [Person] = []  // List of all people
+    var filteredPeople: [Person] = []  // List of filtered people based on search
 
+    // MARK: - viewDidLoad
+    // Initializes the view and sets up the table view and search bar
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Contacts"
         tableView.delegate = self
         tableView.dataSource = self
+        searchBar.delegate = self
+
         people = DatabaseHelper.shared.getAllContacts()
+        filteredPeople = people
     }
 
-    // + Butonu tıklandığında
+    // MARK: - Search Bar Delegate
+    // Filters the contacts based on search text
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            filteredPeople = people
+        } else {
+            filteredPeople = people.filter { person in
+                person.name.lowercased().contains(searchText.lowercased()) ||
+                person.telephone.contains(searchText) ||
+                person.address.lowercased().contains(searchText.lowercased())
+            }
+        }
+        tableView.reloadData()
+    }
+
+    // MARK: - Search Bar Cancel Button
+    // Resets the search bar and shows all people when cancel button is tapped
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        filteredPeople = people
+        tableView.reloadData()
+    }
+
+    // MARK: - Add Person Button
+    // Navigates to the Add Person screen when the "+" button is tapped
     @IBAction func addPersonTapped(_ sender: Any) {
         performSegue(withIdentifier: "showAddPerson", sender: self)
     }
 
-    // Tablo veri kaynakları
+    // MARK: - Table View Data Source
+    // Returns the number of rows in the table view based on filtered people
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return people.count
+        return filteredPeople.count
     }
 
-
+    // MARK: - Table View Cell
+    // Configures each cell with person details
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let person = people[indexPath.row]
+        let person = filteredPeople[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "PersonCell", for: indexPath) as! PersonCell
         cell.nameLabel.text = person.name
         cell.phoneLabel.text = person.telephone
@@ -44,10 +77,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
 
+    // MARK: - QR Button Delegate
+    // Navigates to the QR code screen when the QR button is tapped
     func didTapQRButton(for person: Person) {
         performSegue(withIdentifier: "showQRCode", sender: person)
     }
-    // Silme işlemi
+    
+    // MARK: - Table View Delete Action
+    // Handles deleting a contact when swipe-to-delete is used
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             let personToDelete = people[indexPath.row]
@@ -56,12 +93,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
-    // Segue ile veri alma
+
+    // MARK: - Prepare for Segue
+    // Prepares the data before navigating to another screen
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showAddPerson",
            let destination = segue.destination as? AddPersonViewController {
             destination.delegate = self
-            
+
             if let indexPath = tableView.indexPathForSelectedRow {
                 destination.existingPerson = people[indexPath.row]
                 destination.editIndex = indexPath.row
@@ -72,32 +111,31 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 let selectedPerson = sender as? Person {
             destination.selectedPerson = selectedPerson
         }
-        
     }
-
 }
 
-// Delegate protokolü
+// MARK: - AddPersonDelegate Extension
+// Handles addition and editing of contacts
 extension ViewController: AddPersonDelegate {
     func didAddPerson(_ person: Person) {
         DatabaseHelper.shared.saveContact(person: person)
         people.append(person)
         tableView.reloadData()
     }
+
     func didEditPerson(_ person: Person, at index: Int) {
         let oldPerson = people[index]
         DatabaseHelper.shared.updateContact(oldPerson: oldPerson, newPerson: person)
         people[index] = person
         tableView.reloadData()
     }
-
 }
 
+// MARK: - QR Code Generation
+// Generates a QR code image from a string
 func generateQRCode(from string: String) -> UIImage? {
     if let cgImage = EFQRCode.generate(content: string) {
         return UIImage(cgImage: cgImage)
     }
     return nil
 }
-
-
